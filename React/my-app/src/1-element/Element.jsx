d8ab1a12eb63154aaa949b377026cb32114b475a
coq-j01 <chaebinesxo@gmail.com>
2025-02-03 17:28:52 +0900

    my-app수정

---
+++ React/my-app/src/1-element/Element.jsx
@@ -0,0 +1,41 @@
+// 리액트 엘리먼트(React Element)란?
+// 리액트 엘리먼트는 리액트 앱의 UI를 구성하는 가장 작은 단위입니다. 엘리먼트는 화면에 표시될 내용을 설명하는 자바스크립트 객체로, 컴포넌트를 생성하고 렌더링하는 데 사용됩니다.
+
+// 리액트 엘리먼트는 **불변(immutable)**이며 한 번 생성되면 그 내용을 변경할 수 없습니다. 대신 새로운 엘리먼트를 생성하여 화면의 변화를 반영합니다.
+
+// 리액트 엘리먼트의 특징
+// 1. 불변성: 엘리먼트는 한 번 생성되면 변경할 수 없습니다.
+// 2. 가볍고 빠름: 가상 DOM을 사용하여 변경 사항을 효율적으로 관리합니다.
+// 3. UI의 구성 단위: 컴포넌트 내에서 리액트 엘리먼트는 HTML 태그처럼 사용됩니다.
+
+import React from "react";
+//React 17이상에서는 import하지 않아도 JSX 사용 가능함.
+
+const E1 = <h1 className="greeting">리액트 엘리먼트1입니다.</h1>;
+export const E2 = React.createElement(
+  "h1", //type
+  //null, //props
+  { className: "greeting" },
+  "리액트 엘리먼트2입니다." //[...children]
+);
+// JSX에서 JS시작하려면, {}중괄호 사용(노란색)
+// JS에서 스타일은 KeyVaule객체로 사용 {}중괄호(핑크)
+export const E3 = <h1 style={{ color: "blue" }}>엘리먼트3</h1>; //중괄호 2개
+
+const fruits = ["사과", "바나나", "포도"];
+export const E4 = (
+  <ul>
+    {fruits.map((item, index, array) => {
+      // {JS값표현식}
+      // 리액트의 리스트(목록)은 Key를 줘야 됨.
+      return <li key={index}>{item}</li>; //한줄짜리는 return 생략가능(;,{}도 생략가능)
+    })}
+    {/* <li>사과</li>
+    <li>바나나</li>
+    <li>포도</li> */}
+  </ul>
+);
+
+// 내보내기 할 하나의 컴포넌트(요소)
+export default E1;
+// export { E1, E2 }; //같이 못 씀
---
+++ React/my-app/src/App1.js
@@ -0,0 +1,8 @@
+import E1, { E2, E3, E4 } from "./1-element/Element";
+// E1은 디폴트 E2는 부가적인 거
+
+function App() {
+  return E4;
+}
+
+export default App;
