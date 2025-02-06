e7089dc9bdf3a2d3bcbe913b114c18b395ece4c3
coq-j01 <chaebinesxo@gmail.com>
2025-02-06 13:10:47 +0900

    hook

---
+++ React/my-app/src/App7.js
@@ -0,0 +1,18 @@
+import React from "react";
+import * as E from "./7-hook/Hook";
+import * as Ex from "./7-hook/Ex";
+
+function App() {
+  // return <E.Counter1 />;
+  // return <E.Counter2 />;
+  // return <E.Counter3 />;
+  // return <E.Counter4 />;
+  // return <E.Counter5 />;
+  // return <E.Counter6 />;
+  // return <E.Counter7 />;
+  // return <Ex.Ex1 />;
+  // return <Ex.Ex2 />;
+  return <Ex.Ex3 />;
+}
+
+export default App;
