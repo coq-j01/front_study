bf7d9d8c115df2ecf42e9f0cd65368c5dec2b957
coq-j01 <chaebinesxo@gmail.com>
2025-02-12 13:45:20 +0900

    App14-context 추가

---
+++ React/my-app/src/14-context/ThemeContextProvider.jsx
@@ -0,0 +1,23 @@
+import { useState, useCallback } from "react";
+import ThemeContext from "./ThemeContext";
+import ThemeContextUseContext from "./ThemeContextUseContext";
+
+function ThemeContextProvider() {
+  const [theme, setTheme] = useState("light");
+
+  // useMemo : 값이 저장해 놓음
+  // useCallback : 함수를 저장해 놓음
+  const toggleTheme = useCallback(() => {
+    if (theme == "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, [theme]);
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ThemeContextUseContext />
+    </ThemeContext.Provider>
+  );
+}
+export default ThemeContextProvider;
