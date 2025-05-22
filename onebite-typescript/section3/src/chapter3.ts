77049911f14585ac44be6891250b572998d77440
coq-j01 <chaebinesxo@gmail.com>
2025-05-22 22:30:20 +0900

    Update chapter3.ts

--- onebite-typescript/section3/src/chapter3.ts
+++ onebite-typescript/section3/src/chapter3.ts
@@ -36,3 +36,46 @@ let dog: Dog = {
 
 animal = dog;
 //dog = animal; // 오류발생
+
+type Book = { //슈퍼
+  name: string,
+  price : number,
+}
+
+type ProgrammingBook = { //서브
+  name: string,
+  price:number,
+  skill : string,
+}
+
+let book : Book;
+let programmingBook : ProgrammingBook = {
+  name:"한 입 크기로 잘라먹는 리액트",
+  price : 33000,
+  skill : "reactjs",
+}
+book = programmingBook;
+//programmingBook = book; // 오류발생
+
+/**
+ * 초과 프로퍼티 검사
+ */
+
+let book2 : Book = {
+  name:"한 입 크기로 잘라먹는 리액트",
+  price : 33000,
+  //skill : "reactjs", // 초과 프로퍼티 검사로 인한 오류발생
+}
+
+//변수 활용하여 초과 프로퍼티 검사 피할 수 있음음
+let book3: Book = programmingBook;
+
+function func(book : Book){}
+
+func({
+  name:"한 입 크기로 잘라먹는 리액트",
+  price : 33000,
+  //skill : "reactjs", // 이 경우에도 초과 프로퍼티 검사 실행됨
+})
+
+func(programmingBook); // 변수 사용하면 마찬가지로 가능함
